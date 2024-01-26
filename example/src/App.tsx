e174e603f5933ce4efd83b21a985260fc81b1913
harshw-aumhum <122771880+harshw-aumhum@users.noreply.github.com>
2024-01-26 14:07:16 +0530

    supported props for cameraDevice and microphoneDevice before entering call

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,95 @@
-import React, { CSSProperties, useState } from 'react'
+import React, { CSSProperties, useEffect, useState } from 'react'
 import AgoraUIKit, { layout } from 'agora-custom-ui'
 import 'agora-custom-ui/dist/index.css'
+import AgoraRTC from 'agora-rtc-react'
 
 const App: React.FunctionComponent = () => {
-  const [videocall, setVideocall] = useState(true)
+  const [videocall, setVideocall] = useState(false)
   const [isHost, setHost] = useState(true)
   const [isPinned, setPinned] = useState(false)
   const [username, setUsername] = useState('')
+
+  const [availableCameras, setAvailableCameras] = useState<MediaDeviceInfo[]>(
+    []
+  )
+  const [availableMicrophones, setAvailableMicrophones] = useState<
+    MediaDeviceInfo[]
+  >([])
+  const [selectedCamera, setSelectedCamera] = useState<string | undefined>()
+  const [selectedMicrophone, setSelectedMicrophone] = useState<
+    string | undefined
+  >()
+
+  async function checkAvailableDevices() {
+    const cameras = await AgoraRTC.getCameras()
+    const microphones = await AgoraRTC.getMicrophones()
+    setAvailableCameras(cameras)
+    setAvailableMicrophones(microphones)
+    console.log(await AgoraRTC.getPlaybackDevices())
+  }
+
+  // let localTracks = {
+  //   videoTrack: null,
+  //   audioTrack: null
+  // }
+
+  // var mics = [] // all microphones devices you can use
+  // var cams = [] // all cameras devices you can use
+  // var currentMic // the microphone you are using
+  // var currentCam // the camera you are using
+
+  // let volumeAnimation
+
+  // async function mediaDeviceTest() {
+  //   ;[localTracks.audioTrack, localTracks.videoTrack] = await Promise.all([
+  //     AgoraRTC.createMicrophoneAudioTrack({
+  //       encoderConfig: 'music_standard'
+  //     }),
+  //     AgoraRTC.createCameraVideoTrack()
+  //   ])
+
+  //   // play local track on device detect dialog
+  //   localTracks.videoTrack.play('pre-local-player')
+  //   // localTracks.audioTrack.play();
+
+  //   // get mics
+  //   mics = await AgoraRTC.getMicrophones()
+  //   currentMic = mics[0]
+  //   // add all mics to a select dropdown
+  //   // and keep 0 index item selected
+
+  //   // get cameras
+  //   cams = await AgoraRTC.getCameras()
+  //   currentCam = cams[0]
+  //   // add cameras added to select dropdown
+  //   // and keep 0 index item selected
+  //   // see what value and label will go here
+  // }
+
+  // async function switchCamera(label) {
+  //   // change selected item in select dropdown and
+  //   // switch device of local video track.
+  //   await localTracks.videoTrack.setDevice(currentCam.deviceId)
+  // }
+
+  // async function switchMicrophone(label) {
+  //   // change selected item in select dropdown and
+  //   // switch device of local audio track.
+  //   await localTracks.audioTrack.setDevice(currentMic.deviceId)
+  // }
+
+  // show real-time volume while adjusting device.
+  // function setVolumeWave() {
+  //   volumeAnimation = requestAnimationFrame(setVolumeWave)
+  //   console.log(localTracks.audioTrack.getVolumeLevel() * 100 + '%')
+  // }
+
+  console.log(availableCameras)
+  console.log(availableMicrophones)
+
+  useEffect(() => {
+    checkAvailableDevices()
+  }, [])
 
   return (
     <div style={styles.container}>
@@ -27,16 +110,21 @@ const App: React.FunctionComponent = () => {
             </div>
             <AgoraUIKit
               rtcProps={{
-                appId: 'eac2d689f05d4a79b289fd4832a11082',
+                appId: '',
                 channel: 'test',
                 token: null, // add your token if using app in secured mode
                 role: isHost ? 'host' : 'audience',
                 layout: isPinned ? layout.pin : layout.grid,
                 enableScreensharing: true,
                 enableAudio: false,
-                enableVideo: false
+                enableVideo: false,
+                cameraDeviceId: selectedCamera,
+                microphoneDeviceId: selectedMicrophone
               }}
-              rtmProps={{ username: username || 'user', displayUsername: true }}
+              rtmProps={{
+                username: username || 'user',
+                displayUsername: true
+              }}
               callbacks={{
                 EndCall: () => setVideocall(false)
               }}
@@ -56,6 +144,20 @@ const App: React.FunctionComponent = () => {
             <h3 style={styles.btn} onClick={() => setVideocall(true)}>
               Start Call
             </h3>
+            <select onChange={(e) => setSelectedCamera(e.target.value)}>
+              {availableCameras.map((item, index) => (
+                <option key={index} value={item.deviceId}>
+                  {item.label}
+                </option>
+              ))}
+            </select>
+            <select onChange={(e) => setSelectedMicrophone(e.target.value)}>
+              {availableMicrophones.map((item, index) => (
+                <option key={index} value={item.deviceId}>
+                  {item.label}
+                </option>
+              ))}
+            </select>
           </div>
         )}
       </div>
@@ -90,3 +192,5 @@ const styles = {
 }
 
 export default App
+
+// https://webdemo.agora.io/
